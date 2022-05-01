cd2dbd7c0180da8c5525d752a6d49d58b691ddba
Alejandro Miranda <amiranda110500@gmail.com>
2022-05-01 13:57:12 +0100

    Added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'shoppinglist-1c5e4',
+    appId: '1:165625155300:web:97b82eed79d50d0fcf66f1',
+    storageBucket: 'shoppinglist-1c5e4.appspot.com',
+    apiKey: 'AIzaSyCorTU9d1GMbVg0lwZCvplKlpI5iv17dXo',
+    authDomain: 'shoppinglist-1c5e4.firebaseapp.com',
+    messagingSenderId: '165625155300',
+    measurementId: 'G-ZWF0VTEK14',
+  },
   production: false
 };
 
